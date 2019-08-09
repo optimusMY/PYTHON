f968d668e334f64f1f9fc2ea3fde0fc985eb4c14
optimusMY <53823163+optimusMY@users.noreply.github.com>
2019-08-09 20:14:27 +0300

    Add files via upload

---
+++ sqlite3/sqlite3example.py
@@ -0,0 +1,142 @@
+'''
+import sqlite3
+
+conn = sqlite3.connect('tutorial.db')
+c = conn.cursor()
+
+
+def create_table():
+    c.execute("CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)")
+
+
+def data_entry():
+    c.execute("INSERT INTO stuffToPlot VALUES(1452549219,'2016-01-11 13:53:39','Python',6)")
+
+    conn.commit()
+    c.close()
+    conn.close()
+
+
+create_table()
+data_entry()
+'''
+
+
+'''Dynamic data entry version of previous example
+'''
+import sqlite3
+import time
+import datetime
+import random
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from dateutil import parser
+from matplotlib import style
+style.use('fivethirtyeight')
+
+conn = sqlite3.connect('tutorial.db')
+c = conn.cursor()
+
+
+def create_table():
+    c.execute("CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)")
+
+
+def data_entry():
+    c.execute("INSERT INTO stuffToPlot VALUES(1452549219,'2016-01-11 13:53:39','Python',6)")
+
+    conn.commit()
+    c.close()
+    conn.close()
+
+
+def dynamic_data_entry():
+    unix = int(time.time())
+    date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+    keyword = 'Python'
+    value = random.randrange(0, 10)
+
+    c.execute("INSERT INTO stuffToPlot (unix, datestamp, keyword, value) VALUES (?, ?, ?, ?)",
+              (unix, date, keyword, value))
+
+    conn.commit()
+    time.sleep(1)
+
+
+def read_from_db():
+    c.execute('SELECT * FROM stuffToPlot')
+    data = c.fetchall()
+    print(data)
+    for row in data:
+        print(row)
+
+    c.execute('SELECT * FROM stuffToPlot WHERE value = 3')
+    data = c.fetchall()
+    print(data)
+    for row in data:
+        print(row)
+
+    c.execute('SELECT * FROM stuffToPlot WHERE unix > 1452554972')
+    data = c.fetchall()
+    print(data)
+    for row in data:
+        print(row)
+
+    c.execute('SELECT value, datestamp FROM stuffToPlot WHERE unix > 1452554972')
+    data = c.fetchall()
+    print(data)
+    for row in data:
+        print(row[0])
+
+
+def show_all():
+    c.execute('SELECT * FROM stuffToPlot')
+    data = c.fetchall()
+    [print(row) for row in data]
+
+
+def del_and_update():
+    show_all()
+
+    c.execute('UPDATE stuffToPlot SET value = 99 WHERE value = 3')
+    conn.commit()
+
+    show_all()
+
+    c.execute('DELETE FROM stuffToPlot WHERE value = 99')
+    conn.commit()
+
+    show_all()
+
+
+
+def graph_data():
+    c.execute('SELECT datestamp, value FROM stuffToPlot')
+    data = c.fetchall()
+
+    dates = []
+    values = []
+
+    for row in data:
+        dates.append(parser.parse(row[0]))
+        values.append(row[1])
+
+    plt.plot_date(dates, values, '-')
+    plt.show()
+
+
+create_table()
+dynamic_data_entry()
+time.sleep(1)
+
+read_from_db()
+graph_data()
+del_and_update()
+
+c.close
+conn.close()
+
+
+
+
