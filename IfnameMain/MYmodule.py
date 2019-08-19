73cf4371a3c30afcb3d7acb64685b0589c8d8a61
optimusMY <53823163+optimusMY@users.noreply.github.com>
2019-08-19 18:21:13 +0300

    Add files via upload

---
+++ IfnameMain/MYmodule.py
@@ -0,0 +1,17 @@
+
+def sum(x, y):
+	return x + y
+
+
+print(sum(2, 7)
+print(__name__)
+
+if __name__ == "__main__":
+	print("I am main file now")
+
+
+
+
+
+
+
---
+++ IfnameMain/mainfile.py
@@ -0,0 +1,11 @@
+import MYmodule
+
+print(sum(3, 5))
+
+
+'''
+eger MYmodule icerisinde if __name__ == "__main__":
+kýsmýný eklemezsek yukarýda sum caðýrmamýza raðmen 
+print("I am main file now") iþlenecekti
+
+
